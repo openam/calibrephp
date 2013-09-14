37b2d9c1dc20bdcf941fcdba73b3e57dab42636a
Michael Tuttle <openam@gmail.com>
2013-09-14 16:15:22 -0600

    using https://github.com/slywalker/cakephp-plugin-boost_cake @ a25ab6a59d009a853fb63dae17dbdd6e4bf4ec56 for pagination

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -37,4 +37,10 @@ class AppController extends Controller {
 		'RequestHandler',
 		'Session'
 	);
+
+	public $helpers = array(
+		'Html'      => array('className' => 'BoostCake.BoostCakeHtml'),
+		'Form'      => array('className' => 'BoostCake.BoostCakeForm'),
+		'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
+	);
 }
