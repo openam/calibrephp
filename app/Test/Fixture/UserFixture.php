921f1de9cc1a022ac36e58225a5374dd8830419c
demorfi <demorfi@gmail.com>
2015-03-18 11:04:13 +0600

    Update test fixture for User language

--- app/Test/Fixture/UserFixture.php
+++ app/Test/Fixture/UserFixture.php
@@ -19,6 +19,7 @@ class TagFixture extends CakeTestFixture {
         'modified' => array('type' => 'timestamp', 'null' => true, 'default' => 'CURRENT_TIMESTAMP'),
         'token' => array('type' => 'text', 'null' => false),
         'deny' => array('type' => 'text', 'null' => true),
+        'language' => array('type' => 'text', 'null' => false),
         'indexes' => array(
             'users_idx' => array('column' => 'username', 'unique' => 1)
         ),
@@ -36,14 +37,16 @@ class TagFixture extends CakeTestFixture {
             'name' => 'admin',
             'password' => '7f8d49ccf1392c8b2054623adf7cf396744edeb3',
             'role' => 'admin',
-            'token' => '07cb52e8e776e69d6f99874995ca43fa04d9cfc4'
+            'token' => '07cb52e8e776e69d6f99874995ca43fa04d9cfc4',
+            'language' => 'en'
         ),
         array(
             'id' => 2,
             'name' => 'user',
             'password' => '7f8d49ccf1392c8b2054623adf7cf396744edeb3',
             'role' => 'user',
-            'token' => '3e4978d0d684c1710b100db81cf7d8c379b9e796'
+            'token' => '3e4978d0d684c1710b100db81cf7d8c379b9e796',
+            'language' => 'en'
         ),
         array(
             'id' => 3,
@@ -51,7 +54,8 @@ class TagFixture extends CakeTestFixture {
             'password' => '7f8d49ccf1392c8b2054623adf7cf396744edeb3',
             'role' => 'user',
             'token' => '605afc591d0db1633042dcc4d5c998db7e61fbee',
-            'deny' => 'Erotica, +18'
+            'deny' => 'Erotica, +18',
+            'language' => 'en'
         )
     );
 
